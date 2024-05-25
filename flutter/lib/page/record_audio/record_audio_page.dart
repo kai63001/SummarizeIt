50563976ec33c02dfcd4c64ffb740b6616522393
kai63001 <supanat0245@gmail.com>
2024-05-26 00:20:21 +0700

    feat: update pause and resume

--- flutter/lib/page/record_audio/record_audio_page.dart
+++ flutter/lib/page/record_audio/record_audio_page.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
+// ignore: depend_on_referenced_packages
 import 'package:path/path.dart' as p;
 
 class RecordAudioPage extends StatefulWidget {
@@ -16,14 +18,51 @@ class RecordAudioPage extends StatefulWidget {
 class _RecordAudioPageState extends State<RecordAudioPage> {
   late final AudioRecorder _audioRecorder;
   final player = AudioPlayer();
-
-  bool _isRecording = false;
+  Timer? _timer;
+  int _recordDuration = 0;
+  StreamSubscription<RecordState>? _recordSub;
+  RecordState _recordState = RecordState.stop;
+  StreamSubscription<Amplitude>? _amplitudeSub;
 
   @override
   void initState() {
     super.initState();
     _audioRecorder = AudioRecorder();
+    _recordSub = _audioRecorder.onStateChanged().listen((recordState) {
+      _updateRecordState(recordState);
+    });
+
+    _amplitudeSub = _audioRecorder
+        .onAmplitudeChanged(const Duration(milliseconds: 300))
+        .listen((amp) {
+    });
+
     getFileAllInPath();
+  }
+
+  void _updateRecordState(RecordState recordState) {
+    setState(() => _recordState = recordState);
+
+    switch (recordState) {
+      case RecordState.pause:
+        _timer?.cancel();
+        break;
+      case RecordState.record:
+        _startTimer();
+        break;
+      case RecordState.stop:
+        _timer?.cancel();
+        _recordDuration = 0;
+        break;
+    }
+  }
+
+  void _startTimer() {
+    _timer?.cancel();
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() => _recordDuration++);
+    });
   }
 
   Future<void> getFileAllInPath() async {
@@ -36,23 +75,9 @@ class _RecordAudioPageState extends State<RecordAudioPage> {
     // await player.play(DeviceFileSource('/var/mobile/Containers/Data/Application/A2BF620E-2EE3-46E2-980B-C33F5272216D/Documents/romeo.m4a'));
   }
 
-  Future<String> getFilePath() async {
-    final directory = await getApplicationDocumentsDirectory();
-    final path = directory.path;
-    final randomName = DateTime.now().millisecondsSinceEpoch;
-    return '$path/romeo.m4a';
-  }
-
   Future<void> startRecording() async {
     if (await _audioRecorder.hasPermission()) {
       // Start recording to file
-      final path = await getFilePath();
-      // await _audioRecorder.start(const RecordConfig(encoder: AudioEncoder.pcm16bits),
-      //     path: path);
-      // // ... or to stream
-      // await _audioRecorder
-      //     .startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits));
-
       const encoder = AudioEncoder.aacLc;
 
       if (!await _isEncoderSupported(encoder)) {
@@ -66,10 +91,6 @@ class _RecordAudioPageState extends State<RecordAudioPage> {
 
       // Record to file
       await recordFile(_audioRecorder, config);
-
-      setState(() {
-        _isRecording = true;
-      });
     }
   }
 
@@ -107,16 +128,22 @@ class _RecordAudioPageState extends State<RecordAudioPage> {
   }
 
   Future<void> stopRecording() async {
-    final path = await _audioRecorder.stop();
-    print(path);
+    await _audioRecorder.stop();
+  }
 
-    setState(() {
-      _isRecording = false;
-    });
+  Future<void> pauseRecording() async {
+    await _audioRecorder.pause();
+  }
+
+  Future<void> resumeRecording() async {
+    await _audioRecorder.resume();
   }
 
   @override
   void dispose() {
+    _timer?.cancel();
+    _recordSub?.cancel();
+    _amplitudeSub?.cancel();
     _audioRecorder.dispose();
     super.dispose();
   }
@@ -131,14 +158,59 @@ class _RecordAudioPageState extends State<RecordAudioPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: _isRecording ? stopRecording : startRecording,
-              child: Text(_isRecording ? 'Stop Recording' : 'Start Recording'),
-            ),
+            if (_recordState == RecordState.record) _buildTimer(),
+            _controller(),
             const SizedBox(height: 20),
           ],
         ),
       ),
     );
+  }
+
+  Widget _controller() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        if (_recordState == RecordState.stop)
+          IconButton(
+            icon: const Icon(Icons.mic),
+            onPressed: startRecording,
+          ),
+        if (_recordState == RecordState.record)
+          IconButton(
+            icon: const Icon(Icons.pause),
+            onPressed: pauseRecording,
+          ),
+        if (_recordState == RecordState.pause)
+          IconButton(
+            icon: const Icon(Icons.play_arrow),
+            onPressed: resumeRecording,
+          ),
+        if (_recordState != RecordState.stop)
+          IconButton(
+            icon: const Icon(Icons.stop),
+            onPressed: stopRecording,
+          )
+      ],
+    );
+  }
+
+  Widget _buildTimer() {
+    final String minutes = _formatNumber(_recordDuration ~/ 60);
+    final String seconds = _formatNumber(_recordDuration % 60);
+
+    return Text(
+      '$minutes : $seconds',
+      style: const TextStyle(color: Colors.red),
+    );
+  }
+
+  String _formatNumber(int number) {
+    String numberStr = number.toString();
+    if (number < 10) {
+      numberStr = '0$numberStr';
+    }
+
+    return numberStr;
   }
 }
