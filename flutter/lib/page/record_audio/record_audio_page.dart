dc5f080209d9669b9fbed15fbd47981cc2e6feb1
kai63001 <supanat0245@gmail.com>
2024-05-25 23:40:28 +0700

    feat: record audio finaly work

--- flutter/lib/page/record_audio/record_audio_page.dart
+++ flutter/lib/page/record_audio/record_audio_page.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
+import 'package:path/path.dart' as p;
 
 class RecordAudioPage extends StatefulWidget {
   const RecordAudioPage({super.key});
@@ -12,13 +14,15 @@ class RecordAudioPage extends StatefulWidget {
 }
 
 class _RecordAudioPageState extends State<RecordAudioPage> {
-  final record = AudioRecorder();
+  late final AudioRecorder _audioRecorder;
+  final player = AudioPlayer();
 
   bool _isRecording = false;
 
   @override
   void initState() {
     super.initState();
+    _audioRecorder = AudioRecorder();
     getFileAllInPath();
   }
 
@@ -27,24 +31,41 @@ class _RecordAudioPageState extends State<RecordAudioPage> {
     final path = directory.path;
     final files = Directory(path).listSync();
     print(files);
+    //get file size
+    //play audio
+    // await player.play(DeviceFileSource('/var/mobile/Containers/Data/Application/A2BF620E-2EE3-46E2-980B-C33F5272216D/Documents/romeo.m4a'));
   }
 
   Future<String> getFilePath() async {
     final directory = await getApplicationDocumentsDirectory();
     final path = directory.path;
     final randomName = DateTime.now().millisecondsSinceEpoch;
-    return '$path/$randomName.m4a';
+    return '$path/romeo.m4a';
   }
 
   Future<void> startRecording() async {
-    if (await record.hasPermission()) {
+    if (await _audioRecorder.hasPermission()) {
       // Start recording to file
       final path = await getFilePath();
-      print(path);
-      await record.start(const RecordConfig(), path: path);
-      // ... or to stream
-      await record
-          .startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits));
+      // await _audioRecorder.start(const RecordConfig(encoder: AudioEncoder.pcm16bits),
+      //     path: path);
+      // // ... or to stream
+      // await _audioRecorder
+      //     .startStream(const RecordConfig(encoder: AudioEncoder.pcm16bits));
+
+      const encoder = AudioEncoder.aacLc;
+
+      if (!await _isEncoderSupported(encoder)) {
+        return;
+      }
+
+      final devs = await _audioRecorder.listInputDevices();
+      debugPrint(devs.toString());
+
+      const config = RecordConfig(encoder: encoder, numChannels: 1);
+
+      // Record to file
+      await recordFile(_audioRecorder, config);
 
       setState(() {
         _isRecording = true;
@@ -52,8 +73,42 @@ class _RecordAudioPageState extends State<RecordAudioPage> {
     }
   }
 
+  Future<void> recordFile(AudioRecorder recorder, RecordConfig config) async {
+    final path = await _getPath();
+
+    await recorder.start(config, path: path);
+  }
+
+  Future<String> _getPath() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return p.join(
+      dir.path,
+      'audio_${DateTime.now().millisecondsSinceEpoch}.m4a',
+    );
+  }
+
+  Future<bool> _isEncoderSupported(AudioEncoder encoder) async {
+    final isSupported = await _audioRecorder.isEncoderSupported(
+      encoder,
+    );
+
+    if (!isSupported) {
+      debugPrint('${encoder.name} is not supported on this platform.');
+      debugPrint('Supported encoders are:');
+
+      for (final e in AudioEncoder.values) {
+        if (await _audioRecorder.isEncoderSupported(e)) {
+          debugPrint('- ${encoder.name}');
+        }
+      }
+    }
+
+    return isSupported;
+  }
+
   Future<void> stopRecording() async {
-    await record.stop();
+    final path = await _audioRecorder.stop();
+    print(path);
 
     setState(() {
       _isRecording = false;
@@ -62,7 +117,7 @@ class _RecordAudioPageState extends State<RecordAudioPage> {
 
   @override
   void dispose() {
-    record.dispose();
+    _audioRecorder.dispose();
     super.dispose();
   }
 
--- flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import device_info_plus
 import flutter_secure_storage_macos
 import in_app_purchase_storekit
@@ -14,6 +15,7 @@ import shared_preferences_foundation
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
